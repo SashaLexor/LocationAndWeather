8e13f557a02e7a07073f3cf35f7fa2632bedc1a0
Alex <sashalexor@gmail.com>
2016-10-03 13:47:37 +0300

    Get location
    Use CoreLocation
    Get location after starting app
    Add labels on screen
    Handle some CLErrors

--- LocationAndWeather/LocationViewController.swift
+++ LocationAndWeather/LocationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  LocationViewController.swift
 //  LocationAndWeather
 //
 //  Created by 1024 on 03.10.16.
@@ -7,19 +7,185 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class LocationViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var getWeatherButton: UIButton!
+    @IBOutlet weak var getLocationButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    var location: CLLocation?
+    var updatingLocation = false
+    var lastLocationError: NSError?
+    
+    @IBAction func getWeatherButtonClicked(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func getLocationButtonClicked(_ sender: AnyObject) {
+        if updatingLocation {
+            stopLocationManager()
+        } else {
+            location = nil
+            lastLocationError = nil
+            startLocationManager()
+        }
+        updateLabels()
+        configureButtons()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization() // Allows to get location updates while app is open and the user is interacting with app
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            messageLabel.text = "Location Services Disabled"
+        }
+        
+        startLocationManager()
+        updateLabels()
+        configureButtons()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    func showAllertLocationServicecDenied() {
+        let allert = UIAlertController(title: "Location Services Disabled", message: "Please turn on location services for this app in Settings", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        allert.addAction(action)
+        //self.present(allert, animated: true, completion: nil)
+        present(allert, animated: true, completion: nil)
+    }
+    
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            messageLabel.text = "Updating location"
+            if !updatingLocation {
+                messageLabel.text = "Tap ‘Get Weather’"
+            }
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            let statusMessage: String
+            if let error = lastLocationError {
+                if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue {
+                    statusMessage = "Location services disabled"
+                } else {
+                    statusMessage = "Error getting location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location services disabled"
+            } else {
+                statusMessage = "Updating location"
+            }
+            messageLabel.text = statusMessage
+        }
+    }
+    
+    func configureButtons() {
+        if updatingLocation {
+            getLocationButton.setTitle("Stop", for: .normal)
+            getWeatherButton.isHidden = true
+        } else {
+            getLocationButton.setTitle("Get location", for: .normal)
+            getWeatherButton.isHidden = false
+        }
+    }
+    
+   
+    
+    func startLocationManager() {
+        print("Start Location Manager")
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters // accuracy < 10m
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+            lastLocationError = nil
+            //timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(self.didTimeOut), userInfo: nil, repeats: false)
+        }
+    }
+    
+    func stopLocationManager() {
+        print("Stop Location Manager")
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
+    }
+    
+    
 }
 
+
+// MARK: - CLLocationManagerDelegate
+
+extension LocationViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did fail with error: \(error)")
+        if error._code == CLError.Code.locationUnknown.rawValue {
+            // The location is currently unknown, but Core Location will keep trying.
+            return
+        }
+        lastLocationError = error as NSError?
+        stopLocationManager()
+        updateLabels()
+        configureButtons()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("Did update locations: \(newLocation)")
+        
+        if newLocation.timestamp.timeIntervalSinceNow < -5 || newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+            location = newLocation
+            lastLocationError = nil
+            updateLabels()
+            configureButtons()
+            if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+                print("We get location")
+                stopLocationManager()
+                updateLabels()
+                configureButtons()
+            }
+        }
+    }
+    
+    
+}
